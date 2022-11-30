05fa869561b255cb6e0835abcdcb5d80112faa4a
Marcos Vieira <marcosvieira482@gmail.com>
2022-11-30 19:03:10 -0300

    Estilização da página Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import {View, ScrollView, Text, Image} from "react-native";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
 
 export default function Start() {
     return (
-        <View>
+        <View style={styles.container}>
             <ScrollView showVerticalScrollIndicator={false}>
-                <View>
-                    <Image source={requestAnimationFrame("../../assets/icons/logo3.png")} />
-                    <Text>
-                        Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o melhor nível.
+                <View style={{ alignItems: "center" }}>
+                    <Image source={require("../../assets/icons/logo3.png")} 
+                    style={ styles.logo }/>
+                    <Text style={styles.description}>
+                        Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível.
                     </Text>
                 </View>
 
@@ -16,3 +17,26 @@ export default function Start() {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+
+    description: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+
+    },
+
+
+})
