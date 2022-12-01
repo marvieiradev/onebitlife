b742577c8510c97a783d9ed29b5f96bca07d06c7
Marcos Vieira <marcosvieira482@gmail.com>
2022-12-01 19:23:05 -0300

    Estrutura da página explanation

---
+++ src/Pages/AppExplanation/index.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { ScrollView, View, Text, StyleSheet } from "react-native-gesture-handler";
+
+export default function AppExplanation() {
+
+    function handleSetShowHome() {
+        console.log("BBBBBBB");
+    };
+
+    return (
+
+        <View style={styles.container}>
+            <ScrollView>
+                <View style={{ alignItems: "center" }}>
+                    <Text style={styles.title}>
+                        Antes, deixa {"\n"} eu te explicar...
+                    </Text>
+                    <Text style={styles.descriptionCta}>
+                        Pronto(a) para subir de nível na vida?
+                    </Text>
+                    <Text style={styles.description}>
+                        Na próxima tela você vai poder escolher {"\n"}
+                        seus 4 hábitos de forma individual.
+                    </Text>
+                    <DefaultButton
+                        buttonText={"Continuar"}
+                        handlePress={handleSetShowHome}
+                        width={250}
+                        height={50} />
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: '#FFFFFF',
+        textAlign: "center",
+        marginVertical: 40,
+    },
+
+    descriptionCta: {
+        color: '#FFFFFF',
+        fontWeight: "bold",
+        fontSize: 16,
+        marginTop: 20,
+        marginBottom: 10,
+    },
+
+    description: {
+        color: '#FFFFFF',
+        textAlign: "center",
+        marginBottom: 30,
+    },
+})
